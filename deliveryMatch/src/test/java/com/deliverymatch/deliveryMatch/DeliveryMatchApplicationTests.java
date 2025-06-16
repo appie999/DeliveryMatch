58f08be93df37f7d689f5bf0a417f7b58de205a4
appie999 <aitoutmghartfarid@gmail.com>
2025-06-16 12:03:13 +0100

    first commit

---
+++ deliveryMatch/src/test/java/com/deliverymatch/deliveryMatch/DeliveryMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverymatch.deliveryMatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
